34bf8bbdee211d4c8b19561559df3cbfb9341f52
Huyllee <moriarty1807@gmail.com>
2023-08-29 22:22:31 +0700

    design navbar component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Chat from "./pages/chat";
 import Login from "./pages/login";
 import Register from "./pages/register";
+import { Container } from "react-bootstrap";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Chat />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="8" element={<Navigate to="/" />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Container className="text-secondary">
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="8" element={<Navigate to="/" />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
